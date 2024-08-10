40d9f690bf64c05963a24110693a4baca690b007
NisargOza <oza.nisarg2009@gmail.com>
2024-08-10 18:11:48 -0400

    Fixed CSS

--- app/page.jsx
+++ app/page.jsx
@@ -1,14 +1,10 @@
 import Image from 'next/image';
 import Chat from './components/Chat';
-import { SignUp } from '@clerk/nextjs'
 
 export default function Home() {
   return (
     <>
-              <div className="flex justify-center">
-            <SignUp />
-        </div>
+      <Chat />
     </>
   );
 }
-
